90addc26863ba402f4a520920378f2335125ec65
natsilva <nataliajsilva2@outlook.com>
2021-10-04 18:48:21 -0300

    add arquivo travis

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Natalia de Jesus Silva - Aula DevOps"
+    return "Hello World"
 
 if __name__ == '__main__':
     app.run(debug=True)
