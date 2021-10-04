cda01f2bb670f79e1524f05352fbd129f3fc2e80
natsilva <nataliajsilva2@outlook.com>
2021-10-04 19:38:50 -0300

    corrigindo teste

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Muito obrigada por compartilhar seus conhecimento Prof Gabyy!! <3 
-    "OBS: Você foi a única professora no curso, muitooo feliz em ver mulheres fodass mandando muitooo bem nesse mundão de TI! rsrs"
+    return "Muito obrigada por compartilhar seus conhecimento Prof Gabyy!! <3 OBS: Você foi a única professora no curso, muitooo feliz em ver mulheres fodass mandando muitooo bem nesse mundão de TI! rsrs"
 
 if __name__ == '__main__':
     app.run(debug=True)
